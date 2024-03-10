9712a7981c3cb9ceb0c89fd3d6389fe22b3f080b
D-Guy <ddcool535@gmail.com>
2024-03-09 22:26:46 -0500

    Started millerRabin

---
+++ primailty/millerRabin.cpp
@@ -0,0 +1,10 @@
+// millerRabin.cpp
+// Preforms the Miller-Rabin primaility Test
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a; // n to be tested, a as potential witness
+
+}
