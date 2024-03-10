4da42890c062fcbb546494526dd36c843de0dde1
D-Guy <ddcool535@gmail.com>
2024-03-09 22:04:51 -0500

    Added graphic and fixed division

--- modular/division.cpp
+++ modular/division.cpp
@@ -11,15 +11,19 @@ int main() {
     long long int x, y; // divisor, dividend
     long long int q, r; // quotient, remainder
 
+    cout << "  ____" << endl;
+    cout << "x | y" << endl;
+    cout << "Where x is the divisor and y is the dividend" << endl << endl;
+
     cout << "Input the divisor: ";
     cin >> x;
     cout << "Input the dividend: ";
     cin >> y;
 
-    q = x / y; // Integer division
-    r = x % y; // Remiander
+    q = y / x; // Integer division
+    r = y % x; // Remiander
 
-    cout << x << "/" << y << " = " << q << " remainder " << r << endl;
+    cout << y << "/" << x << " = " << q << " remainder " << r << endl;
     // cout << x << "%" << y << " = " << r << endl;
 }
 
