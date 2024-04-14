094518fefcf10ab8cb80f347064603e5de80bdb8
D-Guy <ddcool535@gmail.com>
2024-04-14 14:10:12 -0400

    Placeholder code

---
+++ hashing/midSquare.cpp
@@ -0,0 +1,14 @@
+// midSquare.cpp
+// Mid-Square hashing algorithm
+// Flawed hash function
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 2;
+
+    for (int i = 10; i <= 40; i++) {
+        cout << i << ": " << i * i << endl;
+    }
+}
